b67be02b5defdcca634cac57fec6e73072694cc1
ArtemiiFrolov <arfr@live.ru>
2018-02-19 19:15:24 +0100

    Working with functions - need to make an analysis

--- parsing_challenge.py
+++ parsing_challenge.py
@@ -7,58 +7,70 @@ import re
 
 geolocator = Nominatim()
 pattern = r"(\d{4})-(\d{2})-(\d{2})"
-with open('parsing_challenge.json') as json_data:
-    data = json.load(json_data)
-sample = random.randint(0, len(data))
-answer_sample = []
 
-#print(data[60720])
-#print (search_dates("Maumee United States of America, 02/09/2016"))
-dates_found = []
-for i in range(10):
-    answer_sample.append([sample+i,  data[sample+i], 'date', 'ranking', 'ranking-address', 'road', 'city', 'country'])
-    #answer_sample.append([92640 + i, data[92640 + i], 'date', 'ranking-date', 'ranking-address', 'road', 'city', 'country'])
-print (answer_sample)
-for i in answer_sample:
+def main(args):
+    with open('parsing_challenge.json') as json_data:
+        data = json.load(json_data)
+    sample = random.randint(0, len(data))
+    answer_sample = []
+    dates_found = []
+    for i in range(10):
+        answer_sample.append(
+            {'N': sample + i, 'address': data[sample + i], 'date': '', 'ranking': '', 'ranking-address': '',
+             'road': '', 'city': '', 'country': ''})
+
+    for i in answer_sample:
+        i = find_date(i)
+        i = find_address(i)
+        print(i)
+    return 2
+
+
+def find_date(one_value):
     try:
-        dates_found = search_dates(i[1])
+        dates_found = search_dates(one_value['address'])
     except ZeroDivisionError:
-        i[1] = i[1].replace('of America', "FixedError")
-        dates_found = search_dates(i[1])
+        one_value['address'] = one_value['address'].replace('of America', "FixedError")
+        dates_found = search_dates(one_value['address'])
 
     if dates_found is not None:
-        i[2] = dates_found[0][1].strftime("%Y-%m-%d")
+        one_value['date'] = dates_found[0][1].strftime("%Y-%m-%d")
         rank = dateparser.date.DateDataParser().get_date_data(dates_found[0][0])
-        i[3] = rank['period']
-        i[1] = i[1].replace(dates_found[0][0],"")
+        one_value['ranking'] = rank['period']
+        one_value['address'] = one_value['address'].replace(dates_found[0][0], "")
     else:
-        i[3] = "No date"
-        tmp = re.search(pattern, i[1])
+        one_value['ranking'] = "No date"
+        tmp = re.search(pattern, one_value['address'])
         if tmp is not None:
-            i[3] = 'day'
-            i[2] = '{}-{}-{}'.format(tmp.group(1),tmp.group(2),tmp.group(3))
+            one_value['ranking'] = 'day'
+            one_value['date'] = '{}-{}-{}'.format(tmp.group(1), tmp.group(2), tmp.group(3))
+    return one_value
 
-    i[1] = i[1].replace("FixedError", 'of America')
-    location = geolocator.geocode(i[1], addressdetails=True)
+
+def find_address(one_value):
+    one_value['address'] = one_value['address'].replace("FixedError", 'of America')
+    location = geolocator.geocode(one_value['address'], addressdetails=True, language='en')
     if location is not None:
         counter = 0
         if 'road' in location.raw['address']:
-            i[5] = location.raw['address']['road']
+            one_value['road'] = location.raw['address']['road']
             counter += 1
         if 'city' in location.raw['address']:
-            i[6] = location.raw['address']['city']
+            one_value['city'] = location.raw['address']['city']
             counter += 1
         if 'country' in location.raw['address']:
-            i[7] = location.raw['address']['country']
+            one_value['country'] = location.raw['address']['country']
             counter += 1
-        if counter==3:
-            i[4] = "Full address"
+        if counter == 3:
+            one_value['ranking-address'] = "Full address"
         else:
-            i[4] = "Part-known address"
-    elif len(i[1])>1:
-        i[4] = "Unknown address"
+            one_value['ranking-address'] = "Part-known address"
+    elif len(one_value['address']) > 1:
+        one_value['ranking-address'] = "Unknown address"
     else:
-        i[4] = "No address"
-    print(i)
+        one_value['ranking-address'] = "No address"
+    return one_value
 
-
+def analysis(one_list):
+    
+a = main(1)
