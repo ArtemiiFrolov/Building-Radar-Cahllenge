7c0e6a084a1207fdb141d40525a3aaaae5779332
ArtemiiFrolov <arfr@live.ru>
2018-02-19 17:32:18 +0100

    Ranking works now

--- parsing_challenge.py
+++ parsing_challenge.py
@@ -12,20 +12,21 @@ answer_sample = []
 #print (search_dates("Maumee United States of America, 02/09/2016"))
 dates_found = []
 for i in range(10):
-    answer_sample.append([sample+i,  data[sample+i], 'date'])
+    answer_sample.append([sample+i,  data[sample+i], 'date', 'ranking'])
     #answer_sample.append([4468 + i, data[33 + i], 'date'])
 for i in answer_sample:
     try:
         dates_found = search_dates(i[1])
-        if dates_found is not None:
-            i[2] = dates_found[0][1].strftime("%Y-%m-%d")
-        print(i)
     except ZeroDivisionError:
         i[1] = i[1].replace('of America', "FixedError")
         dates_found = search_dates(i[1])
-        if dates_found is not None:
-            i[2] = dates_found[0][1].strftime("%Y-%m-%d")
-        print(i)
 
+    if dates_found is not None:
+        i[2] = dates_found[0][1].strftime("%Y-%m-%d")
+        rank = dateparser.date.DateDataParser().get_date_data(dates_found[0][0])
+        i[3] = rank['period']
+    else:
+        i[3] = "No date"
+    print(i)
 
 
